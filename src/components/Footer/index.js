a71cb2ba0f1ca1cb9645f01beb514bf730a05eab
FELIPE TOSCANO MESSIAS <23.85167-8@maua.br>
2024-04-23 20:52:03 -0300

    gamegenius: ajuste css footer e inclusao do link do repo git

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,11 @@ import "./styles.css";
 const Footer = () => { 
  return ( 
  <div> 
-    <footer className="footer"> 
+    <footer className="footer">
+    <a className="span" href=" https://github.com/ToScano23/project_gamegenius" > 
     <span>Projeto Game Genius - IMT TI 2024/01</span> 
-    </footer> 
+    </a>
+    </footer>
  </div> 
  );
 };
