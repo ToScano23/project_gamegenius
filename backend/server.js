4c1127e2970941ea867dd50810541c29cbb6871c
Camila Yamada <reyko.yamada@outlook.com>
2024-04-25 21:55:27 -0300

    post and get implemented

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,61 @@
-const http = require('http')
+require ('dotenv').config()
+
 const express = require('express')
 const app = express()
+const mysql = require('mysql2')
 const porta = 4000
 const bodyParser = require('body-parser')
-const gameSample = [
-    {
-    nome: "Mario Odisey",
-    avaliacao: "5",
-    genero: "Plataforma",
-    plataforma: "Nintendo switch",
-    n_jogadores: "1-2",
-    descricao: "Explore lugares incríveis longe do Reino Cogumelo com o Mario e o novo aliado Cappy em uma imensa aventura 3D ao redor do mundo. Use novas habilidades incríveis – como o poder de capturar e controlar objetos, animais, e inimigos – para coletar Power Moons, ligar a aeronave Odyssey e salvar a princesa Peach de se casar com o Bowser!"
-    }
-]
 
-app.set('port', porta)
-app.set(bodyParser.json())
-
-app.get('/teste', (req, res, next) => {
-    res.send('Hellou')
+const  {DB_USER, DB_PASSWORD, DB_HOST, DB_DATABASE, DB_PORT} = process.env
+const connPool = mysql.createPool({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    database: DB_DATABASE,
+    port: DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 })
 
-app.post('/save-game', (req, res, next) => {
-    const gameSample = req.body
-    gameSample.push({})
-    console.log(game)
-    res.status(201).json(game)
+app.use(bodyParser.json())
+
+// -------------------------------------------
+
+app.get('/teste', (req, res) => {
+    res.send("Foi")
 })
 
-const server = http.createServer(app)
-server.listen(porta)
+app.get('/jogos', (req, res) => {
+    connPool.query("SELECT * FROM gamegenius.jogos", (err, results, fields) => {
+        if (err){
+            console.error("Erro ao realizar consulta", err)
+            res.status(500).send("Erro ao realizar consulta")
+        } else{
+            console.log("Resultados:", results)
+            // console.log("Campos:", fields)
+            res.json(results)
+        }
+    })
+})
+
+app.post('/salvar-jogo', (req, res, next) => {
+    const jogo = req.body
+    const sql = "INSERT INTO gamegenius.jogos (nome, avaliacao, genero, plataforma, n_jogadores, descricao) VALUES (?, ?, ?, ?, ?, ?)"
+    const pars = [jogo.nome, jogo.avaliacao, jogo.genero, jogo.plataforma, jogo.nJogadores, jogo.descricao]
+    
+    connPool.query(sql, pars, (err, results, fields) => {
+        if (err){
+            console.error("Erro ao realizar consulta", err)
+            res.status(500).send("Erro ao realizar consulta")
+        } else{
+            console.log("Dado inserido")
+            // console.log("Campos:", fields)
+            res.json(results)
+        }
+    })
+})
+
+// -------------------------------------------
+
+app.listen(porta, () => console.log("Executando servidor na porta:", porta))
