021c02967793f5127e6326b57552e530db90ca17
Camila Yamada <reyko.yamada@outlook.com>
2024-04-29 09:38:21 -0300

    Add get log e salvar log recebido

--- backend/server.js
+++ backend/server.js
@@ -27,13 +27,40 @@ app.get('/teste', (req, res) => {
 })
 
 app.get('/jogos', (req, res) => {
-    connPool.query("SELECT * FROM gamegenius.jogos", (err, results, fields) => {
+    connPool.query("SELECT * FROM gamegenius.jogos", (err, results) => {
         if (err){
             console.error("Erro ao realizar consulta", err)
             res.status(500).send("Erro ao realizar consulta")
         } else{
             console.log("Resultados:", results)
-            // console.log("Campos:", fields)
+            res.json(results)
+        }
+    })
+})
+
+app.get('/logs', (req, res) => {
+    connPool.query("SELECT * FROM gamegenius.logs", (err, results) => {
+        if (err){
+            console.error("Erro ao realizar consulta", err)
+            res.status(500).send("Erro ao realizar consulta")
+        } else{
+            console.log("Resultados:", results)
+            res.json(results)
+        }
+    })
+})
+
+app.post('/salvar-log', (req, res) => {
+    const log = req.body
+    const query = "INSERT INTO gamegenius.logs (n_players, tipo_multiplayer, genero, plataforma) VALUES (?, ?, ?, ?)"
+    const pars = [log.n_players, log.tipo_multiplayer, log.genero, log.plataforma]
+
+    connPool.query(query, pars, (err, results) => {
+        if (err){
+            console.error("Erro ao realizar consulta", err)
+            res.status(500).send("Erro ao realizar consulta")
+        } else{
+            console.log("Dado inserido com sucesso.")
             res.json(results)
         }
     })
@@ -41,16 +68,15 @@ app.get('/jogos', (req, res) => {
 
 app.post('/salvar-jogo', (req, res, next) => {
     const jogo = req.body
-    const sql = "INSERT INTO gamegenius.jogos (nome, avaliacao, genero, plataforma, n_jogadores, descricao) VALUES (?, ?, ?, ?, ?, ?)"
+    const query = "INSERT INTO gamegenius.jogos (nome, avaliacao, genero, plataforma, n_jogadores, descricao) VALUES (?, ?, ?, ?, ?, ?)"
     const pars = [jogo.nome, jogo.avaliacao, jogo.genero, jogo.plataforma, jogo.n_jogadores, jogo.descricao]
     
-    connPool.query(sql, pars, (err, results, fields) => {
+    connPool.query(query, pars, (err, results) => {
         if (err){
             console.error("Erro ao realizar consulta", err)
             res.status(500).send("Erro ao realizar consulta")
         } else{
-            console.log("Dado inserido")
-            // console.log("Campos:", fields)
+            console.log("Dado inserido com sucesso.")
             res.json(results)
         }
     })
